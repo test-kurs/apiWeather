f8325bf971e9f591245ced186b6d1619096c2821
Jakub D <mrbestkubek@gmail.com>
2019-10-31 18:10:33 +0100

    api weather cz 2

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,5 +1,5 @@
 public class Config {
-    public static final String APP_ID = "963e05d0b9e50c64002b3a9942ed0ac0";
+    public static final String APP_ID = "";
     public static final String APP_URL = "http://api.openweathermap.org/data/2.5/weather";
     public static final String APP_URL_FOR_16DAYS = "http://api.openweathermap.org/data/2.5/forecast/daily";
 
